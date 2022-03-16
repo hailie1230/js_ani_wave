25d402a52cce01a4c6f33809ff38338a2d731457
hailie1230@naver.com <66510673+hailie1230@users.noreply.github.com>
2022-03-16 20:52:14 +0900

    script - wave 속도 수정

--- script.js
+++ script.js
@@ -100,7 +100,7 @@ class Point {
         this.x = x;
         this.y = y;
         this.fixedY = y;
-        this.speed = 0.1;
+        this.speed = 0.08;
         this.cur = index;
         this.max = Math.random() * 100 + 150;
     }
