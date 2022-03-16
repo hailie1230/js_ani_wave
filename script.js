e8c1beb8627e9642617e8acfde6a2b4050dbfd82
hailie1230@naver.com <66510673+hailie1230@users.noreply.github.com>
2022-03-16 20:51:04 +0900

    script - 속도 수정

--- script.js
+++ script.js
@@ -100,7 +100,7 @@ class Point {
         this.x = x;
         this.y = y;
         this.fixedY = y;
-        this.speed = 0.03;
+        this.speed = 0.1;
         this.cur = index;
         this.max = Math.random() * 100 + 150;
     }
